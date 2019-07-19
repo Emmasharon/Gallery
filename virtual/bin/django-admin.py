df0495b266882d8e8d8ac606a80cbc4e516ee1ce
Emma Sharon <emmasharon19@gmail.com>
2019-07-19 11:24:03 +0300

    Create initial file structure

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/emma/Documents/moringa-school-projects/django/gallery-app/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
