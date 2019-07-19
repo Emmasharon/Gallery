6f1364b8670d597f99a521a5711a4bd6884a7e01
Emma Sharon <emmasharon19@gmail.com>
2019-07-19 17:48:08 +0300

    create database

--- gallery/settings.py
+++ gallery/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'emma',
+    'PASSWORD':'1234',
+    }
+}
+TIME_ZONE = 'Africa/Nairobi'
