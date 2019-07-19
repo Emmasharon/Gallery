2c46b7589c98c7a2573bdca607432fbec3739f1b
Emma Sharon <emmasharon19@gmail.com>
2019-07-19 12:12:04 +0300

    install bootstrap

--- pics/views.py
+++ pics/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to my gallery')
+
+def pictures(request):
+    return render(request, 'all-pics/pictures.html')
