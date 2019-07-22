5b4e1d5a3768b903e333d9783fc5cc171cf74615
Emma Sharon <emmasharon19@gmail.com>
2019-07-22 10:30:57 +0300

    write relevant urls

--- pics/urls.py
+++ pics/urls.py
@@ -5,7 +5,9 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
-    url(r'^search/', views.search_results, name='search_results')
+    url(r'^search/', views.search_results, name='search_results'),
+    url(r'^location/(?P<location>\d+)', views.a_location,name='search_location'),
+    url(r'^category/(?P<category>\d+)', views.a_category,name='search_category'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
