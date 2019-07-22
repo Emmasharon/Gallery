cdfa83fe94446315831d7dba79b71a18d8352ab6
Emma Sharon <emmasharon19@gmail.com>
2019-07-22 04:12:37 +0300

    create picture.html template

--- pics/urls.py
+++ pics/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns=[
     url('^$',views.index,name = 'index'),
+    url('^$',views.pictures,name = 'pictures'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- pics/views.py
+++ pics/views.py
@@ -10,4 +10,20 @@ def index(request):
     categories = Category.get_all_categories()
     return render(request,'index.html',{'images':images, 'categories':categories, 'locations':locations})
 
+def pictures(request):
+    images = Image.get_all_images()
+    locations = Location.get_locations()
+    categories = Category.get_all_categories()
+    return render(request,'pictures.html',{'images':images, 'categories':categories, 'locations':locations})
+
+def search_results(request):
+    if 'category' in request.GET and request.GET['category']:
+        search_term = request.GET.get('category')
+        images_found = Image.search_image(search_term)
+        message = f'{search_term}'
+
+        return render(request, 'search.html',{'message':message, 'images':images_found, 'locations':locations})
+    else:
+        message = "Search here"
+        return render(request, 'search.html',{'message':message})
 
